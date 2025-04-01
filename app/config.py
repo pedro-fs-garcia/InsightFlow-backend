336f969b1921b2c871b769896770fbe3f971c409
Pedro Garcia <pedrofsgarcia.pro@gmail.com>
2025-03-31 21:38:34 -0300

    chore: configuração as variáveis de ambiente

--- app/config.py
+++ app/config.py
@@ -3,10 +3,11 @@ import os
 
 load_dotenv()
 
-DB_USER = os.getenv("DB_USER", "default_user")
+DB_USER = os.getenv("DB_USER", "root")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "default_password")
 DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_NAME = os.getenv("DB_NAME", "default_db")
+DB_NAME = os.getenv("DB_NAME", "insightflow")
 DB_PORT = os.getenv("DB_PORT", "3306")
 
-ADMIN_TOKEN = os.getenv("ADMIN_TOKEN")
+BACKEND_SERVER=os.getenv("BACKEND_SERVER", "http://localhost:5000")
+FRONTEND_SERVER=os.getenv("FRONTEND_SERVER", "http://localhost:5173")
